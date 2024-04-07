5be4c4513d23af62cbb77ac740972ab9001b28d4
Siratee K <siratee.k@lmwn.com>
2024-04-07 23:38:31 +0700

    feat: Add TODO

--- cmd/container/main.go
+++ cmd/container/main.go
@@ -1,5 +1,47 @@
 package main
 
-func main() {
+import (
+	"bitcoin-like-guesser/pkg/logic"
+	containerlib "github.com/resource-aware-jds/container-lib"
+	"github.com/resource-aware-jds/container-lib/model"
+	"github.com/resource-aware-jds/container-lib/pkg/containerlibcontext"
+	"github.com/sirupsen/logrus"
+	"strconv"
+)
 
+type TaskData struct {
+	Endpoint     string `json:"endpoint"`
+	ExpectedHash string `json:"expectedHash"`
+	RoundID      string `json:"roundID"`
+	NodeID       string `json:"nodeID"`
+	Start        int    `json:"start"`
+	End          int    `json:"end"`
+}
+
+func main() {
+	containerlib.Run(func(ctx containerlibcontext.Context, task model.Task) error {
+		// TODO: Marshal task attribute into TaskData
+
+		// TODO: Provide HTTPClient
+
+		for i := 0; i < 1000000000; i++ {
+			iStr := strconv.Itoa(i)
+			result := logic.GenerateBase64Hash(iStr)
+
+			// TODO: Use from TaskData
+			if result == cfg.ExpectedHash {
+				// Guess the answer
+				res, err := client.GuessAnswer(iStr)
+				if err != nil {
+					logrus.Error("Guess Answer incorrect!: ", err)
+					return
+				}
+				isWin = res
+				break
+			}
+		}
+
+		ctx.Success()
+		return nil
+	})
 }
